2d0e34cb9374177abff311bfa6f9c7cd963e7022
^x3ro <git@x3ro.dev>
2021-12-10 20:51:37 +0100

    📦 NEW: Finish day 10 part 2

--- src/day-10/day-10.py
+++ src/day-10/day-10.py
@@ -1,13 +1,19 @@
 #%% Input
 
 
+from typing import List
+
+
 def readInp(inp: str):
     with open(inp, "r") as file:
         return [line.strip() for line in file.readlines()]
 
 
 class Stack:
-    _stack = []
+    _stack: List[str]
+
+    def __init__(self):
+        self._stack = []
 
     def push(self, sym: str):
         self._stack.append(sym)
@@ -17,6 +23,12 @@ class Stack:
 
     def peek(self):
         return self._stack[-1]
+
+    def toList(self):
+        return list(reversed(self._stack))
+
+    def __iter__(self):
+        return reversed(self._stack)
 
 
 #%% Part 1
@@ -53,3 +65,46 @@ for line in readInp("input.txt"):
         score += scores[char]
 
 print("Result:", score)
+
+
+#%% Part 2
+
+scores = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
+}
+lineScores = []
+for line in readInp("input.txt"):
+    stack = Stack()
+
+    for char in line:
+        if char == "(":
+            stack.push(")")
+            continue
+        if char == "[":
+            stack.push("]")
+            continue
+        if char == "{":
+            stack.push("}")
+            continue
+        if char == "<":
+            stack.push(">")
+            continue
+
+        # Closing correctly
+        if char == stack.pop():
+            continue
+
+        # Did not close correctly (Syntax error) => Skip line
+        break
+    else:
+        score = 0
+        for char in stack:
+            score *= 5
+            score += scores[char]
+        lineScores.append(score)
+
+finalScore = sorted(lineScores)[len(lineScores) // 2]
+print("Result:", finalScore)
