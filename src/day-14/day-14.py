bf0104ab10b5afeb396fedcbca23750f2a5d174a
^x3ro <git@x3ro.dev>
2021-12-14 08:46:42 +0100

    👌 IMPROVE: 14 / 1: Refactor: Extract methods

--- src/day-14/day-14.py
+++ src/day-14/day-14.py
@@ -29,12 +29,21 @@ def pairs(string: str):
         yield string[i : i + 2]
 
 
-#%% Part 1
+def countElements(polymer: str) -> dict[str, int]:
+    elementCounts = {}
+    for char in polymer:
+        count = elementCounts.get(char, 0)
+        elementCounts[char] = count + 1
+    return elementCounts
 
-polymer, insertions = readInp("input.txt")
-print("Template:", polymer)
 
-for step in range(10):
+def getMaxMinElementCount(elementCounts) -> tuple[int, int]:
+    maxCount = max(elementCounts.values())
+    minCount = min(elementCounts.values())
+    return (maxCount, minCount)
+
+
+def polymerize(polymer: str, insertions: dict[str, str]) -> str:
     offset = 0
     for i, pair in enumerate(pairs(polymer)):
         insertion = insertions.get(pair)
@@ -45,20 +54,27 @@ for step in range(10):
         polymer = polymer[: i + offset + 1] + insertion + polymer[i + offset + 1 :]
         offset += 1
 
+    return polymer
+
+
+#%% Part 1
+
+polymer, insertions = readInp("input.txt")
+print("Template:", polymer)
+
+for step in range(10):
+    polymer = polymerize(polymer, insertions)
+
     if len(polymer) < 30:
         print(f"After step {step}: {polymer}")
     else:
         print(f"After step {step}: {polymer[:30]}... ({len(polymer)})")
 
-elementCounts = {}
-for char in polymer:
-    count = elementCounts.get(char, 0)
-    elementCounts[char] = count + 1
 
+elementCounts = countElements(polymer)
 print(elementCounts)
 
-maxElementCount = max(elementCounts.values())
-minElementCount = min(elementCounts.values())
+maxElementCount, minElementCount = getMaxMinElementCount(elementCounts)
 
 result = maxElementCount - minElementCount
 print()
