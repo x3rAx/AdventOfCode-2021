c231aaf8b51d82be196e456be2a671a83eb4a291
^x3ro <git@x3ro.dev>
2021-12-15 13:31:00 +0100

    👌 IMPROVE: Add alternative solutions for 15 / 1

--- src/day-15/day-15.py
+++ src/day-15/day-15.py
@@ -33,14 +33,9 @@ def buildGraph(cave):
 
 
 def dijkstra(graph, exitIndex: int):
-    # For some reason it just works with `directed=True`... So we mirror the
-    # graph matrix... But what's this? It works even when we do not mirror it???
-    # And it's even faster then!!!
-    # graph[graph == 0] = graph.T[graph == 0]
-
-    dGraph = csr_matrix(graph)
+    sparseGraph = csr_matrix(graph)
     dist_matrix, predecessors = csgraph.dijkstra(
-        csgraph=dGraph, directed=True, indices=exitIndex, return_predecessors=True
+        csgraph=sparseGraph, directed=True, indices=exitIndex, return_predecessors=True
     )
 
     return predecessors
@@ -67,3 +62,35 @@ predecessors = dijkstra(graph, exitIndex=len(cave.flat) - 1)
 costSum = leastRisk(cave, predecessors)
 
 print("Result:", costSum)
+
+
+# --- Alternative 1 ---
+
+# Obviously if you do it right, the `dist_matrix` (even though it is not a
+# matrix), really contains the distances of each node to the exit...
+sparseGraph = csr_matrix(graph)
+dist_matrix = csgraph.dijkstra(
+    csgraph=sparseGraph, directed=True, indices=len(cave.flat) - 1
+)
+print("Alternative 1 - Result:", int(dist_matrix[0]))
+
+
+# --- Alternative 2 (from HoroTW, thanks <3) ---
+
+# ... Or just let `networx` create a grid graph, set weights and let it find
+# the shortest path by weight
+
+from networkx.algorithms.shortest_paths.generic import shortest_path_length
+from networkx.classes.digraph import DiGraph
+from networkx import grid_2d_graph
+
+inp: np.ndarray = np.genfromtxt("input.txt", delimiter=1, dtype=int)
+
+n = len(inp)
+G = grid_2d_graph(n, n, create_using=DiGraph)
+
+for u, v in G.edges:
+    G[u][v]["weight"] = inp[v[1]][v[0]]
+
+print("Alternative 2 - Result:", shortest_path_length(G, (0, 0), (n - 1, n - 1), "weight"))
+
